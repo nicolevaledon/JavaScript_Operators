ad1f7a5e5a1ee3031bd3b1dbd24c71e221bd1b4e
Nicole Valedon <nicole.valedon@gmail.com>
2024-03-29 12:10:55 -0400

    JS Operators Exercise

--- app.js
+++ app.js
@@ -7,13 +7,46 @@ console.log(
 console.log("EXERCISE 1:\n==========\n");
 
 // YOUR CODE HERE
+let a = 20;
+let b = 4;
+let add = a + b;
+let minus = a - b;
+let multiply = a * b;
+let dividing = a / b;
 
 // Exercise 2
 console.log("EXERCISE 2:\n==========\n");
 
 // YOUR CODE HERE
+let num = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Frodo";
+let lastName = "Baggins";
+
+// What is the value of: num + str? "1111"
+// What is the value of: num + str2? "11eleven"
+// What is the value of: num + isPresent? 12
+// What is the value of: firstName + num? "Frodo11"
+//What is the value of: isPresent + str? "true11"
+// What is the value of: firstName + lastName? "FrodoBaggins"
 
 // Exercise 3
 console.log("EXERCISE 3:\n==========\n");
 
 // YOUR CODE HERE
+let val = 5;
+let str3 = "5";
+let str4 = "five";
+let isPresent2 = false;
+
+// What is the value of: val == str3? true
+// What is the value of: val === str3? false
+//What is the value of: !isPresent2? true
+// What is the value of: (“eleven” == str4 && val >= str3)? false
+//What is the value of: (!isPresent2 || isPresent2)? true
+//What is the value of: 0 == false? true
+// What is the value of: 0 === false? false
+//What is the value of: 0 != false? false
+//What is the value of 0 !== false? true
